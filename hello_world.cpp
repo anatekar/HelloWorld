b66fe5097e7dbf70bde8ada8352fda1fd5cbaadd
Amol Natekar <amol.natekar@gmail.com>
2016-11-30 16:58:15 +0530

    Amol:Added version support

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "HelloWorldConfig.h"
 
 int main(int argc,char *argv[])
 {
 	(void)argc;
 	(void)argv;
+	std::cout<<"(CPP) Hello World Version : "<<HELLO_WORLD_MAJOR_NUMBER<<"."<<HELLO_WORLD_MINOR_NUMBER<<std::endl;
 	if(argc == 1){
 		std::cout<<"Hello World!!!\n";
 	}else{
